72271217660674fc00970fd83b2a37500651a60c
471391503@qq.com <lidongjie2008@gmail.com>
2015-10-21 21:38:35 +0800

    创建初始工程，整理目录结构

---
+++ FSImageViewerDemo/FSImageViewerDemo/DemoCode/Controller/DemoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FSImageViewerDemo
+//
+//  Created by ldjhust on 10/21/15.
+//  Copyright © 2015 example. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+
+}
+
